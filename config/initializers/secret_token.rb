954a3c7af7e8e9586d5900aa7ae4386786bf2580
Scott Morgan <morgandev@gmail.com>
2011-05-28 20:30:49 -0700

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LionRoar::Application.config.secret_token = 'd87d6f9ec04717a8b17d0aed8660d885421c4270e2aefec8c7a895cee4907a7e56a29181a40bff9a62f3f212dcd7b679a7645457817710bdc4bbeb7ebace7b3b'
